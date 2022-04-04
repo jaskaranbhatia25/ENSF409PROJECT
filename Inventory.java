1157ff7c81c2b1ee16609972c62d678bea9659fe
Ansh09-chawla <73417447+Ansh09-chawla@users.noreply.github.com>
2022-04-04 15:16:33 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -2,20 +2,20 @@ package edu.ucalgary.ensf409;
 
 public class Inventory{
 	
-	private ArrayList<FoodItem> items = new ArrayList<>();
+	private ArrayList<FoodItem> inventoryItems = new ArrayList<>();
 	
 	public Inventory(){	
 	}
 	public void removeItem(int index){
 		
-		items.set(index -1, null);
+		inventoryItems .set(index -1, null);
 		
 	}	
 	public ArrayList<FoodItem> getItems(){
 		return items;
 	}
     public FoodItem getItem(int index){
-        return items.get(index);
+        return inventoryItems.get(index);
     }		
 	
 }	
