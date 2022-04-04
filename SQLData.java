b3e91aa6d27fa67447bd3afc9616505477760390
aleksander-rudolf <95549699+aleksander-rudolf@users.noreply.github.com>
2022-04-04 17:26:41 -0600

    Add files via upload
    SQLData class

---
+++ SQLData.java
@@ -0,0 +1,50 @@
+package edu.ucalgary.ensf409;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SQLData {
+
+    public final String DBURL;
+    public final String USERNAME;
+    public final String PASSWORD; 
+    private Connection dbConnect;
+    private ResultSet results;
+	
+	private Inventory originalInventory;
+	private Inventory updatedInventory;
+	private ArrayList<ClientType> clients;
+	
+	public SQLData(String dbURL, String username, String password) {
+		this.DBURL = dbURL;
+		this.USERNAME = username;
+		this.PASSWORD = password;
+	}
+	
+	public void connectDatabase() {
+		
+        try{
+            dbConnect = DriverManager.getConnection(this.DBURL, this.USERNAME , this.PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }  
+	}
+	
+	public void updateDatabase() {
+		
+	}
+	
+	public Inventory getOriginalInventory() {
+		return this.originalInventory;
+	}
+	
+	public Inventory getUpdatedInventory() {
+		return this.updatedInventory;
+	}
+	
+	public Clients getClients() {
+		return this.clients;
+	}
+}
