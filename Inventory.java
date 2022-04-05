a1855630d7557724c65db1a14d3cd109a5752c5d
aleksander-rudolf <95549699+aleksander-rudolf@users.noreply.github.com>
2022-04-05 10:01:39 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,27 +1,35 @@
 package edu.ucalgary.ensf409;
 
-public class Inventory{
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Inventory implements Cloneable {
 	
 	private ArrayList<FoodItem> inventoryItems = new ArrayList<>();
 	
-	public Inventory(){	
-	}
+	public Inventory(){}
+	
 	public void removeItem(int index){
-		
-		inventoryItems .set(index -1, null);
-		
+		inventoryItems .set(index -1, null);	
 	}	
+	
 	public ArrayList<FoodItem> getInventoryItems(){
 		return inventoryItems ;
 	}
+	
     public FoodItem getInventoryItem(int index){
         return inventoryItems.get(index);
     }		
 	
-}	
-	
-	
-	
-	
-	
-	
+	public Object clone() throws CloneNotSupportedException {
+		Inventory copy = (Inventory)super.clone();
+		copy.inventoryItems = new ArrayList<FoodItem>();
+		Iterator<FoodItem> iterator = this.inventoryItems.iterator();
+
+		while(iterator.hasNext()) {
+			copy.inventoryItems.add((FoodItem)iterator.next().clone());
+		}
+		return copy;
+	}
+}
