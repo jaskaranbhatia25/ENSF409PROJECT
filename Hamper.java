c57a3e6467720b205f56385bcff7492f0a912dfb
aleksander-rudolf <95549699+aleksander-rudolf@users.noreply.github.com>
2022-04-05 09:59:50 -0600

    Update Hamper.java

--- Hamper.java
+++ Hamper.java
@@ -1,17 +1,19 @@
 package edu.ucalgary.ensf409;
 
+import java.util.*;
+
 public class Hamper{
 	
 	private SQLData myData;
-	private final int TOTAL_WHOLE_GRAIN_CONTENT;
-	private final int TOTAL_FV;
-	private final int TOTAL_PROTEIN;
-	private final int TOTAL_OTHER;
-	private final int TOTAL_CALORIES;
+	private final double TOTAL_WHOLE_GRAIN_CONTENT;
+	private final double TOTAL_FV;
+	private final double TOTAL_PROTEIN;
+	private final double TOTAL_OTHER;
+	private final double TOTAL_CALORIES;
 	private boolean valid = false;
 	private ArrayList<FoodItem> hamperItems = new ArrayList<>();
 	
-	public Hamper( SQLDATA myData, int male, int female,  int under8, int above8){
+	public Hamper(SQLData myData, int male, int female,  int under8, int above8){
 		
 		int maleCalories = myData.getClients().get(0).getCalories();
 		int femaleCalories = myData.getClients().get(1).getCalories();
@@ -21,46 +23,58 @@ public class Hamper{
 		this.TOTAL_WHOLE_GRAIN_CONTENT  = (myData.getClients().get(0).getWholeGrains()*male*0.01*maleCalories
 										+ myData.getClients().get(1).getWholeGrains()*female*0.01*femaleCalories 
 										+ myData.getClients().get(2).getWholeGrains()*under8*0.01*under8Calories 
-										+ myData.getClients().get(3).getWholeGrains()*above8*0.01*over8Calories)*7
+										+ myData.getClients().get(3).getWholeGrains()*above8*0.01*over8Calories)*7;
+		
 	    this.TOTAL_FV   = (myData.getClients().get(0).getFruitVeggies()*male*0.01*maleCalories
 						+ myData.getClients().get(1).getFruitVeggies()*female*0.01*femaleCalories 
 					    + myData.getClients().get(2).getFruitVeggies()*under8*0.01*under8Calories 
-						+ myData.getClients().get(3).getFruitVeggies()*above8*0.01*over8Calories)*7	
+						+ myData.getClients().get(3).getFruitVeggies()*above8*0.01*over8Calories)*7;
+	    
         this.TOTAL_PROTEIN	= (myData.getClients().get(0).getProtein()*male*0.01*maleCalories
 					        + myData.getClients().get(1).getProtein()*female*0.01*femaleCalories 
 					        + myData.getClients().get(2).getProtein()*under8*0.01*under8Calories 
-					        + myData.getClients().get(3).getProtein()*above8*0.01*over8Calories)*7						
+					        + myData.getClients().get(3).getProtein()*above8*0.01*over8Calories)*7;
+        
 		this.TOTAL_OTHER = (myData.getClients().get(0).getOther()*male*0.01*maleCalories
 					    + myData.getClients().get(1).getOther()*female*0.01*femaleCalories 
 					    + myData.getClients().get(2).getOther()*under8*0.01*under8Calories 
-					    + myData.getClients().get(3).getOther()*above8*0.01*over8Calories)*7
-        this.TOTAL_CALORIES =  (maleCalories + femaleCalories + under8Calories + over8Calories)*7;
+					    + myData.getClients().get(3).getOther()*above8*0.01*over8Calories)*7;
 		
+        this.TOTAL_CALORIES =  (maleCalories + femaleCalories + under8Calories + over8Calories)*7;
 	}	
+	
 	public ArrayList<FoodItem> getHamperItems(){
-		return 	hamperItems ;
+		return 	this.hamperItems ;
 	}
-    public int getTotalWholeGrains(){
-        return 	TOTAL_WHOLE_GRAIN_CONTENT;
+	
+    public double getTotalWholeGrains(){
+        return 	this.TOTAL_WHOLE_GRAIN_CONTENT;
     }
-    public int getTotalFV(){
-		return  TOTAL_FV;
+    
+    public double getTotalFV(){
+		return  this.TOTAL_FV;
 	}	
-	public int getTotalProtein(){
-		return TOTAL_PROTEIN;
-	}	
-	public int getTotalOther(){
-		return TOTAL_OTHER ;
-	}	
-	public int getTotalCalories(){
-		return TOTAL_CALORIES ;
+    
+	public double getTotalProtein(){
+		return this.TOTAL_PROTEIN;
 	}
+	
+	public double getTotalOther(){
+		return this.TOTAL_OTHER ;
+	}
+	
+	public double getTotalCalories(){
+		return this.TOTAL_CALORIES ;
+	}
+	
 	public SQLData getMyData(){
-		return myData;
-	}	
+		return this.myData;
+	}
+	
 	public void setMyData(SQLData myData){
 		this.myData = myData;
 	}
+	
     public void updateInventory(){
         Inventory inventory = myData.getUpdatedInventory();	  
 		ArrayList<FoodItem> foodInsideInventory = inventory.getInventoryItems();
@@ -69,15 +83,17 @@ public class Hamper{
 				foodInsideInventory.set(hamperItems.get(i).getItemID()-1, null);
 			}
         }					
-	}	
-	public boolean getValid(){
-		return valid;
 	}
+    
+	public boolean getValid(){
+		return this.valid;
+	}
+	
     public void setValid(boolean validity){
         this.valid = validity;
     }
+    
     public void fillHamper(){
 
     }
-}	
-		
+}
