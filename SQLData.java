a31646a4c43a45398136d0ec1955e22075a9e1e9
Ansh09-chawla <73417447+Ansh09-chawla@users.noreply.github.com>
2022-04-04 20:00:56 -0600

    Update SQLData.java

--- SQLData.java
+++ SQLData.java
@@ -11,7 +11,8 @@ public class SQLData {
         private final String USERNAME;
         private final String PASSWORD; 
         private Connection dbConnect;
-        private ResultSet results;
+        private ResultSet resultsClient;
+	private ResultSet resultsAvailableFood;
 	private Inventory originalInventory;
 	private Inventory updatedInventory;
 	private ArrayList<ClientType> clients;
@@ -27,10 +28,15 @@ public class SQLData {
         try{
             dbConnect = DriverManager.getConnection(this.DBURL, this.USERNAME , this.PASSWORD);
             Statement myStmt = dbConnect.createStatement();
-            results = myStmt.executeQuery("SELECT * FROM "+DAILY_CLIENT_NEEDS);
-	    while (results.next()){
+            resultsClient = myStmt.executeQuery("SELECT * FROM "+DAILY_CLIENT_NEEDS);
+	    while (resultsClient.next()){
 		  ClientType client = new ClientType(getInt("ClientID"), getString("Client"), getInt("WholeGrains"), getInt("FruitVeggies"), getInt("Protein"), getInt("Other"), getInt("Calories"));
 		  this.clients.add(client);
+	    }
+	    while(resultsAvailableFood.next()){
+		    
+	      // we will use clone to create updatedInventory i.e clone originalInventory to updatedInventory	    
+		    
 	    }	    
 	    myStmt.close();    
         } catch (SQLException e) {
@@ -47,9 +53,28 @@ public class SQLData {
           }
         }   
 	
-	public void updateDatabase() {
+	private void delete(int id) {
+         try {
+                String query = "DELETE FROM AVAILABLE_FOOD WHERE ItemID = ?";
+                PreparedStatement myStmt = dbConnect.prepareStatement(query);
+                myStmt.setString(1, id);
+                myStmt.executeUpdate();
+                myStmt.close();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+		
 		
 	}
+	public void updateDatabase(){
+	
+	      for(int i = 0; i< updatedInventory.getInventoryItems().size();i++){
+		   if(updatedInventory.getInventoryItems().get(i)==null){
+			   delete(i+1);
+		   }
+	      }	      	
+	}	
 	
 	public Inventory getOriginalInventory() {
 		return this.originalInventory;
