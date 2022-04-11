9d354618c649c9bc210e5d9f08f811bcb9bb6e30
aleksander-rudolf <95549699+aleksander-rudolf@users.noreply.github.com>
2022-04-10 20:28:20 -0600

    Update SQLData.java

--- SQLData.java
+++ SQLData.java
@@ -1,3 +1,9 @@
+/**
+@author Aleksander Rudolf, Ansh Singh, Jaskaran Bhatia
+@version 2.0
+@since 1.0 - Mar. 28/2022
+*/
+
 package edu.ucalgary.ensf409;
 
 import java.util.*;
@@ -5,9 +11,9 @@ import java.sql.*;
 
 public class SQLData {
 
-	private final String DBURL;
-    private final String USERNAME;
-    private final String PASSWORD; 
+	private String DBURL = new String();
+    private String USERNAME= new String();
+    private String PASSWORD= new String(); 
     private Connection dbConnect;
     private ResultSet resultsClient;
 	private ResultSet resultsAvailableFood;
@@ -15,32 +21,50 @@ public class SQLData {
 	private Inventory updatedInventory=new Inventory();;
 	private ArrayList<ClientType> clients=new ArrayList<>();
 	
+	/**
+	 * 
+	 * @param dbURL
+	 * @param username
+	 * @param password
+	 */
 	public SQLData(String dbURL, String username, String password) {
 		this.DBURL = dbURL;
 		this.USERNAME = username;
 		this.PASSWORD = password;
 	}
+	
+	public SQLData() {}
+	
+	/*
+	 * 
+	 */
 	public void refreshUpdatedInventoryWhenOrderFails() throws CloneNotSupportedException {
 		Inventory refreshedUpdatedInventory = (Inventory)originalInventory.clone();
 		this.updatedInventory = refreshedUpdatedInventory;
 	}
+	
+	/*
+	 * 
+	 */
 	public void refreshOriginalInventoryWhenOrderSuccess() throws CloneNotSupportedException {
 		Inventory refreshedOriginalInventory = (Inventory)updatedInventory.clone();
 		this.originalInventory = refreshedOriginalInventory;
 	}	
 	
+	/*
+	 * 
+	 */
 	public void connectDatabase() throws CloneNotSupportedException {
-		
         try {
             dbConnect = DriverManager.getConnection(this.DBURL, this.USERNAME , this.PASSWORD);
             Statement myStmt = dbConnect.createStatement();
             resultsClient = myStmt.executeQuery("SELECT * FROM " + "DAILY_CLIENT_NEEDS");
-            while (resultsClient.next()){
+            while (resultsClient.next()) {
             	ClientType client = new ClientType(resultsClient.getInt("ClientID"), resultsClient.getString("Client"), resultsClient.getInt("WholeGrains"), resultsClient.getInt("FruitVeggies"), resultsClient.getInt("Protein"), resultsClient.getInt("Other"), resultsClient.getInt("Calories"));
             	this.clients.add(client);
             }	
             resultsAvailableFood = myStmt.executeQuery("SELECT * FROM " + "AVAILABLE_FOOD");
-            while(resultsAvailableFood.next()){
+            while(resultsAvailableFood.next()) {
             	FoodItem foodItem = new FoodItem(resultsAvailableFood.getInt("ItemID"), resultsAvailableFood.getString("Name"), resultsAvailableFood.getInt("GrainContent"), resultsAvailableFood.getInt("FVContent"), resultsAvailableFood.getInt("ProContent"), resultsAvailableFood.getInt("Other"), resultsAvailableFood.getInt("Calories"));
             	this.originalInventory.getInventoryItems().add(foodItem);
             }
@@ -51,8 +75,10 @@ public class SQLData {
         }  
 	}
 	
+	/*
+	 * 
+	 */
 	public void close() {
-        
 		try {
             resultsClient.close();
             resultsAvailableFood.close();
@@ -62,6 +88,10 @@ public class SQLData {
           }
 	}   
 	
+	/**
+	 * 
+	 * @param id
+	 */
 	private void delete(int id) {
          try {
         	 String query = "DELETE FROM AVAILABLE_FOOD WHERE ItemID = ?";
@@ -74,25 +104,36 @@ public class SQLData {
          }
 	}
 	
-	public void updateDatabase(){
-		
-
-	
-		for(int i = 0; i< updatedInventory.getInventoryItems().size();i++){
-			if(updatedInventory.getInventoryItems().get(i).getName().equals("")){
+	/*
+	 * 
+	 */
+	public void updateDatabase() {
+		for(int i = 0; i< updatedInventory.getInventoryItems().size();i++) {
+			if(updatedInventory.getInventoryItems().get(i).getName().equals("")) {
 				delete(i+1);
 			}
 		}	      	
 	}	
 	
+	/**
+	 * 
+	 * @return Inventory
+	 */
 	public Inventory getOriginalInventory() {
 		return this.originalInventory;
 	}
 	
+	/**
+	 * 
+	 * @return Inventory
+	 */
 	public Inventory getUpdatedInventory() {
 		return this.updatedInventory;
 	}
-	
+	 /**
+	  * 
+	  * @return ArrayList<ClientType>
+	  */
 	public ArrayList<ClientType> getClients() {
 		return this.clients;
 	}
