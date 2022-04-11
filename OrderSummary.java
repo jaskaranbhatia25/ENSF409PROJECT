46f698055cf0694dfc01ac12637c8900e917a1c6
aleksander-rudolf <95549699+aleksander-rudolf@users.noreply.github.com>
2022-04-10 20:28:02 -0600

    Update OrderSummary.java

--- OrderSummary.java
+++ OrderSummary.java
@@ -1,6 +1,17 @@
+/**
+@author Aleksander Rudolf, Ansh Singh, Jaskaran Bhatia
+@version 2.0
+@since 1.0 - Mar. 28/2022
+*/
+
 package edu.ucalgary.ensf409;
 
 public interface OrderSummary {
 
+	/**
+	 * 
+	 * @param numOfOrders
+	 * @return String
+	 */
 	String formatSummary(int numOfOrders);
 }
